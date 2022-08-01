0756011ff0fcde18ac17ecd0061784b3b6d19cc4
Aleksandrs Petruhins <aleksandrs.petruhins@gmail.com>
2022-08-01 20:58:23 +0300

    First commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Aleksandrs Petruhins on 01/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Honeymoon
+Honeymoon app from Udemy course SwiftUI Masterclass 2022 - iOS App Development &amp; Swift
